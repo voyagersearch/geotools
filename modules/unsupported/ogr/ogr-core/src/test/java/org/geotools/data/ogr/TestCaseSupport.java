c14725432f088061136a9b42c2dd1e8b3481bda5
jdeolive <jdeolive@gmail.com>
2012-04-22 13:29:05 -0400

    Reverting changes to TestCaseSupport to pass in class rather than instanbce, and creating stub test-data directories in child modules to make sample data tests work

--- modules/unsupported/ogr/ogr-core/src/test/java/org/geotools/data/ogr/TestCaseSupport.java
+++ modules/unsupported/ogr/ogr-core/src/test/java/org/geotools/data/ogr/TestCaseSupport.java
@@ -233,11 +233,11 @@ public abstract class TestCaseSupport extends TestCase {
      */
     protected void copy(final String name, String[] requiredExtensions, String[] optionalExtensions) throws IOException {
         for (int i = 0; i < requiredExtensions.length; i++) {
-            assertTrue(TestData.copy(TestCaseSupport.class, sibling(name, requiredExtensions[i])).canRead());
+            assertTrue(TestData.copy(this, sibling(name, requiredExtensions[i])).canRead());
         }
         for (int i = 0; i < optionalExtensions.length; i++) {
             try {
-                assertTrue(TestData.copy(TestCaseSupport.class, sibling(name, optionalExtensions[i])).canRead());
+                assertTrue(TestData.copy(this, sibling(name, optionalExtensions[i])).canRead());
             } catch (FileNotFoundException e) {
                 // Ignore: this file is optional.
             }
@@ -256,7 +256,7 @@ public abstract class TestCaseSupport extends TestCase {
             copy(testData, new String[] { "shp", "dbf", "shx" }, new String[] { "prj" });
         else if (testData.endsWith(".MIF"))
             copy(testData, new String[] { "MIF", "MID" }, new String[0]);
-        File f = new File(TestData.url(TestCaseSupport.class, testData).getFile());
+        File f = new File(TestData.url(this, testData).getFile());
         return f.getAbsolutePath();
     }
 
